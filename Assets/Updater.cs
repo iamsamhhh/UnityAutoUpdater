0f99333bc257a1b0dd8318ede82595f7199f3c93
iamsamhhh <sc_samchen@icloud.com>
2022-03-11 18:49:03 +0800

    Version detector implemented

--- Assets/Updater.cs
+++ Assets/Updater.cs
@@ -1,18 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Net;
+using System.IO;
 using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 public class Updater : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public const string version = "a.0.0.1";
+
+    
+    void Awake()
     {
-        
+        if (CheckForUpdate()){
+            // client.DownloadFile("https://github.com/iamsamhhh/UnityAutoUpdater/releases/download/" )
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    bool CheckForUpdate(){
+        HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://api.github.com/repos/iamsamhhh/UnityAutoUpdater/releases");
+        request.UserAgent = "something";
+        HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+        string htmlString;
+        using (var reader = new StreamReader(response.GetResponseStream()))
+        {
+            htmlString = reader.ReadToEnd();
+        }
+        Debug.Log(htmlString);
+        if (htmlString.Contains(version))
+        return true;
+        else
+        return false;
     }
+
+#if UNITY_EDITOR
+    [MenuItem("AutoUpdater/Export configuration %e", false, 1)]
+    private static void ExportConfiguration(){
+        SaveMgr.instance.Save(version, "version");
+    }
+#endif 
+
+    
 }
